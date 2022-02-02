a57b01078efd0bbe812a99a1b54db3138fafa443
Pratik <pratik.vaity@1silverbullet.tech>
2022-02-02 15:24:27 +0530

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
